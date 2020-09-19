4ff06ca9de23b0bc38c061a39e3c839928688865
Theetouch Kasemarnontana <58279552+lisbono2001@users.noreply.github.com>
2020-09-19 19:54:02 +0700

    Fix setting STATIC_URL error

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,4 +117,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/style.css'
+STATIC_URL = '/static/style.css/'
